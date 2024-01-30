7916324413fd776488160b1de122c6012e97e8e8
Jared Eller <eller.jared@student.greenriver.edu>
2024-01-30 18:17:07 -0500

    Added a route, added a view, added an image.

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
 <?php
+    /*  Jared Eller
+     *  January 30, 2024
+     *  https://github.com/jEllerGRC/diner
+     *  This is my CONTROLLER. MVC: Model, View, Controller.
+     */
 
-echo "Diner";
+    //Turn on error reporting
+    ini_set("display_errors", 1);
+    error_reporting(E_ALL);
+
+    //Require the autoload file
+    require_once("vendor/autoload.php");
+
+    //Instantiate Fat-Free framework (F3)
+    $f3 = Base::instance(); //static method call
+    //Java translation: Base f3 = new Base();
+
+    //Define a default route
+    $f3 -> route ("GET / ", function()
+    { //anonymous function call as an argument for route
+//        echo "My Diner";
+
+        //Display a view page
+        $view = new Template();
+        echo $view -> render ("views/home.html");
+    });
+
+    //Run F3
+    $f3 -> run(); //instance method call
 
 ?>
