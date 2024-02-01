8ca99cd2d0e87472b3031c3504e3043dfa41caba
Jared Eller <eller.jared@student.greenriver.edu>
2024-02-01 03:27:18 -0500

    Added views for the order pages and the order form.

--- index.php
+++ index.php
@@ -36,6 +36,67 @@
         echo $view -> render ("views/breakfast-menu.html");
     });
 
+    //Define the order form 1 route
+    $f3 -> route ("GET|POST /order1", function($f3)
+    { //anonymous function call as an argument for route
+        echo "Order Form Part I";
+
+        //if the form has been posted
+        if($_SERVER["REQUEST_METHOD"] == "POST")
+        {
+            //validate the data
+            $food = $_POST["food"];
+            $meal = $_POST["meal"];
+
+            //put the data in the session array
+            $f3 -> set ("SESSION.food", $food);
+            $f3 -> set ("SESSION.meal", $meal);
+
+            //redirect to order2 route
+            $f3 -> reroute ("order2");
+
+        }
+
+        //Display a view page
+        $view = new Template();
+        echo $view -> render ("views/order-form-1.html");
+    });
+
+    //Define the order form 2 route
+    $f3 -> route ("GET|POST /order1", function($f3) { //anonymous function call as an argument for route
+//        echo "Order Form Part II";
+
+        //if the form has been posted
+        if ($_SERVER["REQUEST_METHOD"] == "POST")
+        {
+            //validate the data
+            $food = $_POST["food"];
+            $meal = $_POST["meal"];
+
+            //put the data in the session array
+            $f3->set("SESSION.food", $food);
+            $f3->set("SESSION.meal", $meal);
+
+            //redirect to summary route
+            $f3->reroute("summary");
+
+        }
+
+        //Display a view page
+        $view = new Template();
+        echo $view -> render ("views/order-form-2.html");
+    });
+
+    //Define a breakfast route
+    $f3 -> route ("GET /summary", function()
+    { //anonymous function call as an argument for route
+    //            echo "breakfast";
+
+        //Display a view page
+        $view = new Template();
+        echo $view -> render ("views/order-summary.html");
+    });
+
     //Run F3
     $f3 -> run(); //instance method call
 
