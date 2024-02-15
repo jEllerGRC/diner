a338167436f92b9a5b28f7a087ab414e51fd77c2
Jared Eller <eller.jared@student.greenriver.edu>
2024-02-15 13:50:56 -0500

    Moved more routing functions from index.php to controller.php

--- index.php
+++ index.php
@@ -37,64 +37,15 @@
     });
 
     //Define a breakfast route
-    $f3 -> route ("GET /breakfast", function()
-    { //anonymous function call as an argument for route
-//            echo "breakfast";
-
-        //Display a view page
-        $view = new Template();
-        echo $view -> render ("views/breakfast-menu.html");
+    $f3 -> route ("GET /breakfast", function($con)
+    {
+        $con -> breakfast();
     });
 
     //Define the order form 1 route
-    $f3 -> route ("GET|POST /order1", function($f3)
-    { //anonymous function call as an argument for route
-//        echo "Order Form Part I";
-
-        //Initialize variables to ensure proper scope
-        $food = "";
-        $meal = "";
-
-        //if the form has been posted
-        if($_SERVER["REQUEST_METHOD"] == "POST")
-        {
-            //validate the data
-            if(validFood($_POST["food"]))
-            {
-                $food = $_POST["food"];
-            }
-            else
-            {
-                $f3 -> set("errors['food']", "Invalid food");
-            }
-
-            if(isset($_POST["meal"]) and validMeal($_POST["meal"]))
-            {
-                $meal = $_POST["meal"];
-            }
-            else
-            {
-                $f3 -> set("errors['meal']", "Invalid meal choice");
-            }
-
-            if (empty($f3 -> get("errors")))
-            {
-                //If there aren't any errors, instantiate an Order object.
-                $order = new Order($food, $meal);
-
-                //put the Object in the session array
-                $f3 -> set ("SESSION.order", $order);
-
-                //redirect to order2 route
-                $f3 -> reroute ("order2");
-            }
-        }
-
-        //Add data to the f3 "hive"
-        $f3 -> set("meals", DataLayer::getMeals()); //make a method call to something stored in the data-layer file
-
-        $view = new Template();
-        echo $view->render('views/order-form-1.html');
+    $f3 -> route ("GET|POST /order1", function($con)
+    {
+        $con -> order1();
     });
 
     //Define the order form 2 route
